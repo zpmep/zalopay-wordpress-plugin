0ffaf06dca966e339eeaa97c3888bfd7e113c65c
Duy Nguyen <ndhduy.7798@gmail.com>
2021-03-02 11:17:55 +0700

    Update class-wc-zlp-api.php

--- zlp/includes/class-wc-zlp-api.php
+++ zlp/includes/class-wc-zlp-api.php
@@ -2,7 +2,7 @@
 if (!defined('ABSPATH')) {
 	exit;
 }
-
+date_default_timezone_set ('Asia/Ho_Chi_Minh');
 /**
  * WC_Stripe_API class.
  *
@@ -10,7 +10,6 @@ if (!defined('ABSPATH')) {
  */
 class WC_ZaloPay_API
 {
-	date_default_timezone_set ('Asia/Ho_Chi_Minh');
 	/**
 	 * Stripe API Endpoint
 	 */
